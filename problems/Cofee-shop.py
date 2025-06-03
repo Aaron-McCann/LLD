1c235fc2b08064744590b286b97b29a679908f60
Aaron-McCann <aamccann@tcd.ie>
2025-06-03 22:42:00 +0100

    Started Cofee Shop

---
+++ problems/Cofee-shop.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+
+class Coffee(ABC):
+
+    def __init_(self):
+        self.description = "unknown coffee"
+
+    def get_description(self):
+        return self.description
+    
+    @abstractmethod
+    def get_cost(self)
+        pass
+
+    def get_ingredients(self):
+        return self.get_description
+    
+
+class Espresso(Coffee):
+    def __init_(self):
+        super().__init__()
+        self.description = "Espresso"
+
+    def get_cost(self):
+        return 1.99
